7be676fd24856f1ddcd231cda73cbd829e2545e3
ThyagarajR <107422709+ThyagarajR@users.noreply.github.com>
2022-09-06 15:21:27 +0530

    Update tomcatDeploy.groovy

--- vars/tomcatDeploy.groovy
+++ vars/tomcatDeploy.groovy
@@ -3,10 +3,9 @@ def call(credId,userName,ipAddrs){
             
             ipAddrs.each{ipAddr->
   sh "mv target /*.war target/webapp.war"
-            sh "scp -o StrictHostKeyChecking=no target/webapp.war ${userName}@${ipAddr}:/opt/tomcat9/webapps/"
-            sh "ssh ${userName}@${ipAddr} /opt/tomcat9/bin/shutdown.sh"
+  sh "scp -o StrictHostKeyChecking=no target/webapp.war ${userName}@${ipAddr}:/opt/tomcat9/webapps/"
+  sh "ssh ${userName}@${ipAddr} /opt/tomcat9/bin/shutdown.sh"
   sh "ssh ${userName}@${ipAddr} /opt/tomcat9/bin/startup.sh"
-  sh "ssh ${userName}@${ipAddr} /opt/tomcat9/bin/startup.sh"
-}
             }
-            }
+         }
+     }
